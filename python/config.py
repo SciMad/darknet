a63ced76986d9283fd1e49a1d82c93c34809bb6f
Madhav Humagain <scimad2.71@gmail.com>
2019-08-05 14:17:19 +0545

    Implement paths using config file

---
+++ python/config.py
@@ -0,0 +1,6 @@
+static = {  "darknet-root": "/home/madhav/Desktop/Structure-Sensor/Work/Robotic-Vision/darknet/",
+            "lib_path" : "libdarknet.so",
+            "weights_path" : "yolov3.weights",
+            "config_path" : "cfg/yolov3.cfg",
+            "meta_path" : "cfg/coco.data",
+            }
